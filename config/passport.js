aa03021fe242e6310998e2c4f2a532463d94ec96
Yalcincan <41164799+veotaar@users.noreply.github.com>
2023-10-06 00:09:54 +0300

    Add passport config

---
+++ config/passport.js
@@ -0,0 +1,40 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('../models/user');
+const bcrypt = require('bcryptjs');
+
+// passport will call this
+const verifyCallback = async (username, password, done) => {
+  try {
+    const user = await User.findOne({ username: username });
+    if (!user) {
+      // username does not exist
+      return done(null, false, { message: 'Incorrect username or password' });
+    }
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+      // passwords do not match!
+      return done(null, false, { message: 'Incorrect username or password' });
+    }
+    return done(null, user);
+  } catch (err) {
+    return done(err);
+  }
+}
+
+const strategy = new LocalStrategy(verifyCallback);
+
+passport.use(strategy);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
