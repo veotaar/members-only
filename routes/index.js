d25982b38e4ea4d813e57a5156664be6c68730e4
Yalcincan <41164799+veotaar@users.noreply.github.com>
2023-10-07 17:36:25 +0300

    Add signup and login views
    with their get routes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/signup', (req, res) => res.render('sign-up-form', {title: 'Create Account'}));
+
+router.get('/login', (req, res) => res.render('login-form', {title: 'Login'}));
+
 module.exports = router;
